13d5341e67573bbc538203747bc7d597b03a1af6
mihdenis85 <mihdenis777@yandex.ru>
2022-02-07 19:08:49 +0500

    change size with pg_up or pg_down

--- maps_ai.py
+++ maps_ai.py
@@ -20,6 +20,36 @@ class Map_params:
         self.dictionary['spn'] = ",".join([str(self.spn), str(self.spn)])
         return self.dictionary
 
+    def spn_up(self):
+        if 0.01 >= self.spn > 0.001:
+            self.spn -= 0.001
+        elif 0.1 >= self.spn > 0.01:
+            self.spn -= 0.01
+        elif 1 >= self.spn > 0.1:
+            self.spn -= 0.1
+        elif 10 >= self.spn > 1:
+            self.spn -= 1
+        elif 90 >= self.spn > -80:
+            self.spn -= 10
+
+    def spn_down(self):
+        if 0.01 >= self.spn > 0.001:
+            self.spn += 0.001
+        elif 0.1 >= self.spn > 0.01:
+            self.spn += 0.01
+        elif 1 >= self.spn > 0.1:
+            self.spn += 0.1
+        elif 10 >= self.spn > 1:
+            self.spn += 1
+        elif 80 >= self.spn > -90:
+            self.spn += 10
+
+    def update(self, key):
+        if key == pygame.K_PAGEUP:
+            self.spn_up()
+        if key == pygame.K_PAGEDOWN:
+            self.spn_down()
+
 
 def maps_ai_1(dictionary):
     api_server = "http://static-maps.yandex.ru/1.x/"
@@ -33,26 +63,23 @@ def maps_ai_1(dictionary):
         with open(NAME_OF_FILE, "wb") as file:
             file.write(response.content)
 
-def import_map():
+def import_map(param_for_req):
     pygame.init()
     screen = pygame.display.set_mode((600, 450))
-    # Рисуем картинку, загружаемую из только что созданного файла.
-    screen.blit(pygame.image.load(NAME_OF_FILE), (0, 0))
-    # Переключаем экран и ждем закрытия окна.
-    pygame.display.flip()
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYUP:
+                param_for_req.update(event.key)
+                maps_ai_1(param_for_req.make_dict())
+        screen.blit(pygame.image.load(NAME_OF_FILE), (0, 0))
+        pygame.display.flip()
     pygame.quit()
 
 if __name__ == '__main__':
     params_save_1 = Map_params(longitude, lattitude)
-    my_dict = params_save_1.make_dict()
-    maps_ai_1(my_dict)
-    import_map()
-
-
-
-
-
+    import_map(params_save_1)
 
 
